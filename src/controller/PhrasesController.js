d62a19ab2cebfb326b01c49bdca92b2951b94307
Marcos Paiva <eu.marcos.paiva@gmail.com>
2019-09-29 23:50:16 -0300

    Initializing texts (#5)
    * Define phrase structure
    
    * Create phrases components
    
    * Refact node patterns
    
    * require phrases methods and test then

--- src/controller/PhrasesController.js
+++ src/controller/PhrasesController.js
@@ -1,4 +1,12 @@
+const {getAdjectives, getPronoums, getPrefix} = require('../phrases');
+
+console.log( getPrefix('masc') );
+
 module.exports = {
+
+    // Phrase model
+    // Prefix + Pronoum [fem | masc] + adjective;
+
     show(req, res) {
         res.send('Hello world');
     }
---
+++ src/phrases.js
@@ -0,0 +1,95 @@
+const getPronoums = (gender) => {
+    const mascPronoums = [
+        'meu',
+        'nosso'
+    ]
+
+    const femPronoums = [
+        'minha',
+        'nossa'
+    ]
+
+    if (gender == 'masc') {
+        return mascPronoums;
+    } else {
+        return femPronoums;
+    }
+};
+
+const getPrefix = () =>  {
+    const preffix = [
+        'Fala',
+        'Opa',
+        'Bom dia',
+        'Oi',
+        'Hello'
+    ];
+
+    return preffix;
+};
+
+const getAdjectives = (gender) => {
+    const mascAdjectives = [
+        'Campeão',
+        'Federal',
+        'Batizado',
+        'Aspirante',
+        'Apóstolo',
+        'Pontual',
+        'Companheiro',
+        'Balaústre',
+        'Acionista',
+        'Promotor',
+        'Patrão',
+        'Incorreto',
+        'Peregrino',
+        'Estrategista',
+        'Tributarista',
+        'Comediante',
+        'Politeísta',
+        'Supra-sumo',
+        'Maestro',
+        'Influencer',
+        'Desbravador',
+        'Tupi-guarani',
+        'Sensei'
+    ];
+
+    const femAdjectives = [
+        'Campeã',
+        'Federal',
+        'Batizada',
+        'Aspirante',
+        'Apóstola',
+        'Pontual',
+        'Companheira',
+        'Balaústre',
+        'Acionista',
+        'Promotora',
+        'Patroa',
+        'Incorreta',
+        'Peregrina',
+        'Estrategista',
+        'Tributarista',
+        'Comediante',
+        'Politeísta',
+        'Supra-sumo',
+        'Maestra',
+        'Influencer',
+        'Desbravadora',
+        'Tupi-guarani',
+        'Sensei'
+    ]
+
+    if (gender == 'masc') {
+        return mascAdjectives;
+    } else {
+        return femAdjectives;
+    }
+};
+
+module.exports = {
+    getPrefix,
+    getPronoums,
+    getAdjectives
+}
