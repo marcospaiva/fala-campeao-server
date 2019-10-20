c7540664dd438546c19047fefa46c83249f27230
Marcos <marcos@nuvemshop.com.br>
2019-10-20 20:32:34 -0300

    Change postcss preset env

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('postcss-preset-env')({
-            stage: 3,
+            stage: 1,
         }),
         require('postcss-nested'),
         require('autoprefixer'),
