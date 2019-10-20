cf5302918e6fa7eedd5bb318d8898b5ea01433bc
Marcos <marcos@nuvemshop.com.br>
2019-10-20 20:32:48 -0300

    Create Header.js

---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <span role="img" aria-label="Hello">👋</span>
+                Bem-vindx ao Fala Campeão
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
