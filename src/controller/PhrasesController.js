14d9150e29728c4f709a2ed908b50a0ab5b2396e
Marcos <marcos@nuvemshop.com.br>
2020-08-29 11:32:27 -0300

    Controller show phrase and other gender

--- src/controller/PhrasesController.js
+++ src/controller/PhrasesController.js
@@ -7,18 +7,22 @@ module.exports = {
     // Prefix + Pronoum [fem | masc] + adjective;
 
     show(req, res) {
-        const genderByQueryParam = req.query.gender == 'f' ? 'fem' : 'masc';
-        const otherGenderByQueryParam = req.query.gender == 'f' ? 'masc' : 'fem';
-        const adjective = randomizeArray(getAdjectives( genderByQueryParam ));
-        const prefix = randomizeArray(getPrefix( genderByQueryParam ));
-        const pronoum = randomizeArray(getPronoums( genderByQueryParam ));
-        const otherGenderAdjective = randomizeArray(getAdjectives( otherGenderByQueryParam ));
-        const randomPhrase = `${prefix}, ${pronoum} ${adjective}`;
+        const genderByQueryParam = req.query.gender == 'f' ? 'fem' : 'masc'
+        const otherGenderByQueryParam = req.query.gender == 'f' ? 'masc' : 'fem'
+        const adjective = randomizeArray(getAdjectives( genderByQueryParam ))
+        const prefix = randomizeArray(getPrefix( genderByQueryParam ))
+        const pronoum = randomizeArray(getPronoums( genderByQueryParam ))
+
+        const otherGenderAdjective = getAdjectives( otherGenderByQueryParam )
+        const otherGenderPrefix = getPrefix( otherGenderByQueryParam )
+        const otherGenderPronoum = getPronoums( otherGenderByQueryParam )
+
+        const randomPhrase = `${prefix.value}, ${pronoum.value} ${adjective.value}`
+        const randomPhraseOther = `${otherGenderPrefix[prefix.id]}, ${otherGenderPronoum[pronoum.id]} ${otherGenderAdjective[adjective.id]}`
 
         const greeting = {}
         greeting.phrase = randomPhrase
-        greeting.adjective = adjective;
-        greeting.otherGender = otherGenderAdjective;
+        greeting.phraseOtherGender = randomPhraseOther
 
         res.json(greeting);
     }
