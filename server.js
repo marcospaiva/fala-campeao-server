ed0b3b0fb57a68f040fd864cdf676af5e861dfa5
Marcos <marcos@nuvemshop.com.br>
2020-12-26 12:46:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 app.use('/api', require('./src/routes'));
 
 let port = process.env.PORT || 3001;
